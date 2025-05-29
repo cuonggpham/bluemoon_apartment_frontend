47e0866d73ae97fe47ea30e32c920c269ee56bf1
Nguyen Bui Tuan Linh <linh.nbt225732@sis.hust.edu.vn>
2025-05-29 17:48:55 +0700

    feat: add Utility Bill upload functionality and integrate with management page

--- src/pages/dashboard/mainContent/UtilityBills.tsx
+++ src/pages/dashboard/mainContent/UtilityBills.tsx
@@ -1,15 +1,34 @@
 import Row from "../../../components/Row";
 import Heading from "../../../components/Heading";
-import UtilityBill from "../../../components/UtilityBill";
+import Modal from "../../../components/Modal";
+import UtilityBillList from "../../../components/UtilityBillList";
+import UtilityBillUpload from "../../../components/UtilityBillUpload";
+import { useState } from "react";
 
 export default function UtilityBills() {
+  const [refreshTrigger, setRefreshTrigger] = useState(0);
+
+  const handleUploadSuccess = () => {
+    // Trigger refresh of the list
+    setRefreshTrigger(prev => prev + 1);
+  };
+
   return (
-    <>
+    <Modal>
       <Row type="horizontal">
         <Heading as="h1">Utility Bills Management</Heading>
+        <Modal.Open id="uploadUtilityBill">
+          <button className="upload-utility-btn">
+            Upload Utility Bill +
+          </button>
+        </Modal.Open>
       </Row>
       
-      <UtilityBill />
-    </>
+      <UtilityBillList refreshTrigger={refreshTrigger} />
+      
+      <Modal.Window id="uploadUtilityBill" name="Upload Utility Bill">
+        <UtilityBillUpload onSuccess={handleUploadSuccess} />
+      </Modal.Window>
+    </Modal>
   );
 }
